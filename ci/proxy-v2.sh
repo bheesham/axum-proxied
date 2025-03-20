de2014740382fcb75af87d1c81b8b8a6e034cf15
Bheesham Persaud <git@me.bheesham.com>
2025-03-20 00:54:07 -0400

    server is maybe sleepy?

--- ci/proxy-v2.sh
+++ ci/proxy-v2.sh
@@ -3,11 +3,12 @@ set -e
 
 cargo build --example proxy
 cargo run --example proxy & SERVER_PID=$!
+sleep 3
 haproxy -f dev/haproxy.conf & HAPROXY_PID=$!
+sleep 3
 
 echo "running proxy example ($SERVER_PID)"
 echo "  haproxy ($HAPROXY_PID)"
-sleep 3
 
 set +e
 sh dev/test-proxy-v2.sh
