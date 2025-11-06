fb2be2cedbe694032d2622e4d5713c438ec351e3
Bheesham Persaud <git@me.bheesham.com>
2025-11-06 00:36:00 -0500

    fix ci chill on the fuzz logs

--- ci/fuzz.sh
+++ ci/fuzz.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
 
-cargo fuzz run proxy_parse -- -max_total_time=30
-cargo fuzz run extract_forwarded_interface -- -max_total_time=30
+cargo fuzz run proxy_parse -- -verbosity=0 -max_total_time=30
+cargo fuzz run extract_forwarded_interface -- -verbosity=0 -max_total_time=30
